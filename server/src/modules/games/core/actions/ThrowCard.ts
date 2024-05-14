6ca1f60a1e933ece5e3d1062bb96e50fab7288be
Matias F. Devecchi Di Bella <matiasfdevecchi@gmail.com>
2024-05-13 23:25:54 -0300

    feat: throw card

---
+++ server/src/modules/games/core/actions/ThrowCard.ts
@@ -0,0 +1,19 @@
+import { UserId } from "../../../users/core/domain/User";
+import { Card } from "../domain/Cards";
+import { Game, GameStep } from "../domain/Game";
+import { GameNotifier, gameNotifier } from "../domain/GameNotifier";
+import { GameService, gameService } from "../domain/GameService";
+
+export class ThrowCard {
+    constructor(private gameService: GameService, private gameNotifier: GameNotifier) { }
+
+    public async invoke(id: Game['id'], userId: UserId, card: Card, step: GameStep): Promise<void> {
+        const game = await this.gameService.getUserGame(id, userId);
+        const updatedGame = game.throwCard(userId, card, step);
+        const savedGame = await this.gameService.update(updatedGame);
+        const newEvents = savedGame.getNewEvents(game);
+        this.gameNotifier.notifyNewEvents(savedGame.getPlayersIds(), newEvents);
+    }
+}
+
+export const throwCard = new ThrowCard(gameService, gameNotifier);
